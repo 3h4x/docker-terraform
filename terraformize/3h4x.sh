13b18b75a8e4524398f5a9aa0f04b26bbe84507f
31337 <ochach@gmail.com>
2014-08-03 21:31:50 +0200

    AWS can has strange charz in SECRET

--- terraformize/3h4x.sh
+++ terraformize/3h4x.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-sed -i "s/AWS_ACCESS_KEY/$AWS_ACCESS_KEY/g" /terraformize/terraformize.tf
-sed -i "s/AWS_SECRET_KEY/$AWS_SECRET_KEY/g" /terraformize/terraformize.tf
+#!/bin/bash -x
+sed -i "s!AWS_ACCESS_KEY!$AWS_ACCESS_KEY!g" /terraformize/terraformize.tf
+sed -i "s!AWS_SECRET_KEY!$AWS_SECRET_KEY!g" /terraformize/terraformize.tf
